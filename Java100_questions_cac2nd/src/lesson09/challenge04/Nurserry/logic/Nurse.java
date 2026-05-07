01ca51755a618b9c01131a1e37197ef0846e189b
hikaruk <hikaruk@C010423221.zipangu.cac.co.jp>
2026-05-07 23:16:25 +0900

    lesson9
    100本ノック完了

--- Java100_questions_cac2nd/src/lesson09/challenge04/Nurserry/logic/Nurse.java
+++ Java100_questions_cac2nd/src/lesson09/challenge04/Nurserry/logic/Nurse.java
@@ -1,22 +1,32 @@
 package lesson09.challenge04.Nurserry.logic;
 
+import lesson09.challenge04.Nurserry.exception.CriticalConditionException;
+
 public class Nurse {
-    private String name;
+	private String name;
 
-    public Nurse(String name) {
-        this.name = name;
-    }
+	public Nurse(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-
-    //ここに適切な処理を記述
-
+	//ここに適切な処理を記述
+	public void takeTemperature(NurserySchoolChild child)
+			throws CriticalConditionException {//例外が起こるかもの宣言
+		if (child.getTemperature() >= 38) {
+			System.out.println("\n" + child.getName() + "は重体です。");
+			throw new CriticalConditionException();//例外発生を表す処理終了
+		} else {
+			System.out.println("\n" + child.getName() + "の体温は"
+					+ child.getTemperature() + "度です。");
+		}
+	}
 
 }
